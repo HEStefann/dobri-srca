64ff5e6f9e8e2969c1726024a3794b55cd4c3f4d
HEStefann <stefanhadjieftimov@gmail.com>
2023-12-01 22:21:48 +0100

    Update datebase

--- database/migrations/2023_12_01_203621_create_users_table.php
+++ database/migrations/2023_12_01_203621_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('phone_number');
             $table->boolean('anonymous')->default(false);
             $table->unsignedBigInteger('role_id');
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->foreign('role_id')->references('id')->on('roles');
